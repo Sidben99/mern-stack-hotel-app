a9a7319f69d07a0d0f1e9f2a6885b5f1480b05e9
Sidben99 <sidben9991@gmail.com>
2026-09-23 23:09:24 +0100

    refactor(protectedRoute): add role check to protected route

--- frontend/src/components/protectedRoute.tsx
+++ frontend/src/components/protectedRoute.tsx
@@ -2,19 +2,24 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useUserStore } from '@/store/userStore';
 import { toast } from 'sonner';
 import { useEffect } from 'react';
-
-export default function ProtectedRoute() {
+import { ROLES } from '@lankaStay/shared/consts/roles';
+export default function ProtectedRoute({ roles }: { roles: ROLES[] }) {
   const authState = useUserStore((state) => state.authState);
 
   useEffect(() => {
     if (!authState) {
       toast.error('unauthorized access');
     }
-  }, [authState]);
+    if (authState && !roles.includes(authState.user.role)) {
+      toast.error('forbidden access');
+    }
+  }, [authState, roles]);
 
   if (!authState) {
     return <Navigate to="/auth/login" replace />;
   }
-
+  if (authState && !roles.includes(authState.user.role)) {
+    return <div> forbidden access </div>;
+  }
   return <Outlet />;
 }
